3a5ad08c8044b80ec5dba88ef467e5a2e0be87f4
Shubh Bansal <shubhb016@gmail.com>
2026-01-03 09:42:10 +0400

    Initial static website for Shubh Laabh Logistics

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
+import { HomePage } from './pages/HomePage';
+import { ServicesPage } from './pages/ServicesPage';
+import { AboutPage } from './pages/AboutPage';
+import { WhyUsPage } from './pages/WhyUsPage';
+import { ContactPage } from './pages/ContactPage';
+import { ThemeProvider } from './components/ThemeProvider';
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="light">
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/services" element={<ServicesPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/why-us" element={<WhyUsPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
